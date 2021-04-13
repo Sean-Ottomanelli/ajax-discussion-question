7d364feef3cc15ffeca53204ad8924a97b03df1d
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-04-13 09:51:27 -0400

    First part done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 const fullname = document.getElementById("fullname");
-fullname.innerText = "test"
+
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+let button = document.querySelector("button");
+
+
+button.addEventListener("click", () => {
+
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then((response) => {
+      let name = response.results[0].name.first + " " + response.results[0].name.last;
+      // debugger
+      // console.log(finalArray)
+      fullname.innerText = name
+    })
+
+
+
+})
+
