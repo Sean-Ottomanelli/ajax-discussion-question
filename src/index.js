92bea8dde2427c3a812ffc96ef1205439b58c860
github username <github email address>
2021-04-13 09:41:25 -0400

    Seanding from Sean to Katie driver switch

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fullname = document.getElementById("fullname");
+fullname.innerText = "test"
 console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
 
 document.addEventListener("DOMContentLoaded", () => {
